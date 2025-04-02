48edfbf1d224fbd39be2e1568b236374a59e1b63
Joao Otavio <joaootaviocampestrini@gmail.com>
2025-04-01 21:22:57 -0300

    primeiro commit

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Account } from 'src/accounts/entities/account.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @OneToMany(() => Account, (account) => account.user)
+  accounts: Account[];
+}
